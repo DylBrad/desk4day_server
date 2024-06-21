677dad76d43477cd0a0b33be006bf681bffb0fc9
DylBrad <dylanbradley1@hotmail.com>
2024-06-21 19:26:19 +0100

    Server test

--- src/api/logReviews.js
+++ src/api/logReviews.js
@@ -14,6 +14,8 @@ router.post('/', async (req, res, next) => {
 
     logEntry.reviews.push(reviewData);
 
+    console.log('------->>>>>>>', logEntry);
+
     const updatedLogEntry = logEntry.save();
 
     res.json(updatedLogEntry);
